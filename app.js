80ae649d6963800d1e68dda89cc2eedcc7c23a17
KD <kuldeeparyadotcom@gmail.com>
2017-03-22 08:14:18 +0000

    Implement DELETE route to enable user to delete specific timetable items

--- app.js
+++ app.js
@@ -42,6 +42,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/users', users);
 app.use('/timetableitem', timetableitem);
 app.use('/timetableitems', timetableitems);
--- routes/timetableitem.js
+++ routes/timetableitem.js
@@ -42,3 +42,67 @@ module.exports = router;
 //     "description": "Dummy Task 2",
 //     "status": "started"
 // }
+
+
+router.patch('/:id', function (req, res, next) {
+    TimetableItem.findById(req.params.id, function (err, timetableitem) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        if (!timetableitem) {
+            return res.status(500).json({
+                title: 'No timetableitem Found!',
+                error: {message: 'Timetable Item not found'}
+            });
+        }
+        timetableitem.start_time = req.body.start_time;
+        timetableitem.end_time = req.body.end_time;
+        timetableitem.description = req.body.description;
+        timetableitem.status = req.body.status;
+        timetableitem.save(function(err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Updated message',
+                obj: result
+            });
+        });
+    });
+});
+
+
+router.delete('/:id', function (req, res, next) {
+    TimetableItem.findById(req.params.id, function (err, timetableitem) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        if (!timetableitem) {
+            return res.status(500).json({
+                title: 'No timetableitem Found!',
+                error: {message: 'Timetable Item not found'}
+            });
+        }
+        timetableitem.remove(function(err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Deleted message',
+                obj: result
+            });
+        });
+    });
+});
