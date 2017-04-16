600b3b8b440be36b347a0432835469492ebf6c8e
KD <kuldeeparyadotcom@gmail.com>
2017-04-16 08:16:20 +0000

    Secure routes against jwt token

--- routes/timetableitems.js
+++ routes/timetableitems.js
@@ -8,6 +8,20 @@ var dbConfig = config.get('timetable.dbConfig');
 
 var mongoose = require('mongoose');
 
+var jwt = require('jsonwebtoken');
+
+router.use('/', function(req, res, next){
+    jwt.verify(req.query.token, 'superSecretKey', function(err, decoded) {
+        if (err) {
+            return res.status(401).json({
+                title: 'Not Authenticated',
+                error: err
+            });
+        }
+        next();
+    })
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     TimetableItem.find({}, function(err, docs) {
